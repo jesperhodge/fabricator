b9ff85eabc271f20f58a4f5998598cedcce55b0b
Jesper Hodge <jesper@instaffo.de>
2018-06-16 15:14:39 +0200

    #13 delete unnecessary stuff

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'home#project'
+  root to: 'home#index'
   match '*path', to: 'home#index', via: :get
 end
