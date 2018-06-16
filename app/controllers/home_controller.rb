09e4bd9e0012edd080a08d7b202ae70f850d9556
Jesper Hodge <jesper@instaffo.de>
2018-06-16 15:45:57 +0200

    #13 still trying to get rspec running

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def test
+    render json: 'Tested.'
+  end
 end
---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+Rspec.describe 'test rspec itself' do
+  it 'tests' do
+    expect(1).to eq(1)
+  end
+end
