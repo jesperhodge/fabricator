35fb896d5d02387555d78570e280346953445b5a
Jesper Hodge <jesper@instaffo.de>
2018-06-17 22:07:30 +0200

    #14 add migration for authlogic

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Admin.create(email: 'developer@fabricator.com', password: 'fabricator1234')
+
+## Not working yet due to Authlogic Changes
+## Admin.create(email: 'developer@fabricator.com', password: 'fabricator1234')
