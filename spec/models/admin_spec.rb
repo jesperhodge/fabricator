3574328d420266300510b33dee298891d2ff8d41
Jesper Hodge <jesper@instaffo.de>
2018-07-14 20:42:50 +0200

    #14 fix last tests

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe Admin, type: :model do
   describe 'create' do
     it 'tests that everything works' do
-      described_class.create(email: 'jes')
-      expect(described_class.last.first_name).to eq('Jesper')
+      admin = described_class.create(email: 'jesper@host.com', password: 'pass9999', password_confirmation: 'pass9999')
+      expect(described_class.last.email).to eq('jesper@host.com')
     end
   end
 
