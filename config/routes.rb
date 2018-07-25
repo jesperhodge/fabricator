5aa66b7ac0dfbb791f7dc4749a098654fef31258
Angelo Cordon <angelocordon@gmail.com>
2018-07-24 21:58:34 -0700

    Define landing_page#index as root route for unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'home#index'
+  root to: 'landing_page#index'
 
   resources :users, only: [] do
     collection do
